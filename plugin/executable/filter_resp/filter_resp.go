3ce66880c5b5314b0d476a67c63680e021569af5
nonegg <none@none.no>
2026-04-22 17:49:17 +0800

    filter_resp: executable plugin that filter rrtype

---
+++ plugin/executable/filter_resp/filter_resp.go
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2020-2022, IrineSistiana
+ *
+ * This file is part of mosdns.
+ *
+ * mosdns is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * mosdns is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package filter_resp
+
+import (
+	"context"
+	"fmt"
+	"github.com/IrineSistiana/mosdns/v5/pkg/query_context"
+	"github.com/IrineSistiana/mosdns/v5/plugin/executable/sequence"
+	"github.com/miekg/dns"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+const (
+	PluginType = "filter_resp"
+)
+
+func init() {
+	sequence.MustRegExecQuickSetup(PluginType, QuickSetup)
+}
+
+var _ sequence.Executable = (*FilterResp)(nil)
+
+type FilterResp struct {
+	rrtype uint16
+}
+
+func QuickSetup(bq sequence.BQ, s string) (any, error) {
+	ts := strings.TrimSpace(s)
+	ti, err := strconv.Atoi(ts)
+	if err != nil {
+		return nil, fmt.Errorf("filter_resp needs one integer argument")
+	}
+
+	return NewFilterResp(uint16(ti)), nil
+}
+
+func NewFilterResp(rrt uint16) *FilterResp {
+	return &FilterResp{rrt}
+}
+
+func (f *FilterResp) Exec(ctx context.Context, qCtx *query_context.Context) error {
+	if r := qCtx.R(); r != nil {
+		r.Answer = slices.DeleteFunc(r.Answer, func(rr dns.RR) bool {
+			return rr.Header().Rrtype == f.rrtype
+		})
+	}
+	return nil
+}
